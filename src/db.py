26875a241e65d682d05cd44cd01e905b91456ef9
noahwinston <nwinston96@gmail.com>
2018-04-05 17:18:06 -0400

    os.environ.get

--- src/db.py
+++ src/db.py
@@ -3,7 +3,7 @@ import traceback
 import os
 import psycopg2
 
-DATABASE_URL = os.environ('DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 def remove_message(msg_id):
     query = 'DELETE FROM MESSAGES WHERE MessageID = %s'
