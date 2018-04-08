3dbe0e35d1eae747a149a8b518639328c94fe2cb
noahwinston <nwinston96@gmail.com>
2018-04-08 14:21:36 -0400

    Changed most_unique_reacts_on_a_post

--- src/db.py
+++ src/db.py
@@ -121,6 +121,7 @@ def _exists_in_message_reacts(conn, msg_id, react_name):
     c.execute("SELECT * FROM MessageReacts WHERE MessageReacts.MessageID = %s AND MessageReacts.ReactName = %s",
                        (msg_id, react_name))
     result = c.fetchone()
+    print(result)
     if not result:
         return False
     return True
@@ -132,6 +133,7 @@ def _exists_in_user_reacts(conn, user_id, react_name):
         "SELECT * FROM UserReacts WHERE UserReacts.UserID = %s AND UserReacts.ReactName = %s",
         (user_id, react_name))
     result = c.fetchone()
+    print(result)
     if not result:
         return False
     return True
