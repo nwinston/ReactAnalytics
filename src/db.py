d90a2697e2a187d781c1e1818902b2b2a6e6f0dc
noahwinston <nwinston96@gmail.com>
2018-04-08 14:28:50 -0400

    Changed most_unique_reacts_on_a_post

--- src/db.py
+++ src/db.py
@@ -227,12 +227,12 @@ def get_messages_by_user(user_id):
 def get_message_text(team_id, msg_id, conn=None):
 
     query = "SELECT MessageText FROM Messages WHERE Messages.MessageID = %s"
-    text = ''
     if conn is None:
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     c = conn.cursor()
     c.execute(query, (msg_id, ))
     result = c.fetchone()
+    print(result)
     if not result:
         return ''
 
