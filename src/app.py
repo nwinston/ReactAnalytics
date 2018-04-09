4d746d76047320ef0f91b7de14f188593f8142dd
noahwinston <nwinston96@gmail.com>
2018-04-08 20:57:38 -0400

    Changed most_unique_reacts_on_a_post

--- src/app.py
+++ src/app.py
@@ -80,7 +80,7 @@ def get_help_response():
                                 bot.MOST_UNIQUE_REACTS_ON_POST, '[_optional_ *@User*]',
                                 bot.MOST_REACTED_TO_MESSAGES, '[_optional_ *@User*]',
                                 bot.REACT_BUZZWORDS, '[_required_ :react:, :react2: ...]',
-                                bot.MOST_REACTS
+                                bot.MOST_REACTS, ''
                                 )
 
 @app.route('/react_analytics', methods=['GET', 'POST'])
