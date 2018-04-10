a74adb999ab29451d47c15964b81314367b0127b
noahwinston <nwinston96@gmail.com>
2018-04-09 21:50:24 -0400

    Changed most_unique_reacts_on_a_post

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from multiprocessing import Queue
-from multiprocessing import Process, Lock
+from threading import Thread, Lock
 import re
 from slackclient import SlackClient
 import analytics
@@ -46,8 +46,8 @@ class Bot(object):
 		# credentials we set earlier in our local development environment.
 
 		Bot.load_users()
-		Bot.event_thread = Process(target=Bot.event_handler_loop)
-		Bot.event_thread.run()
+		Bot.event_thread = Thread(target=Bot.event_handler_loop)
+		Bot.event_thread.start()
 
 
 	'''
