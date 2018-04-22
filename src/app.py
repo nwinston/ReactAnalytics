a57eee6b1f2684607aa32afa6a224ec5692b67f8
noahwinston <nwinston96@gmail.com>
2018-04-22 12:16:21 -0400

    Changed most_unique_reacts_on_a_post

--- src/app.py
+++ src/app.py
@@ -86,6 +86,7 @@ def on_slash_command():
         return make_response(get_help_response(), 200)
     if text.split(' ')[0] in VALID_COMMANDS:
         task = queue_bot_event.delay(slash_command['token'], EVENT_TYPE_SLASH_COMMAND, slash_command)
+        print('waiting...')
         task.wait()
         if not task:
             response_text = 'Invalid token'
