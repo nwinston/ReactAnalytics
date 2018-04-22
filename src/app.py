8209e16b6e585d9f8d96679e5cedcc846c5eb954
noahwinston <nwinston96@gmail.com>
2018-04-22 11:45:53 -0400

    Changed most_unique_reacts_on_a_post

--- src/app.py
+++ src/app.py
@@ -9,8 +9,20 @@ app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = os.getenv('REDIS_URL', 'redis://localhost:6379')
 app.config['CELERY_RESULT_BACKEND'] = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
 
 @app.route("/install", methods=['GET'])
 def pre_install():
