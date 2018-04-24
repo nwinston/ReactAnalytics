093b3142098de7e7b7f3c6cc8b10829237a7c3b5
noahwinston <nwinston96@gmail.com>
2018-04-24 17:53:09 -0400

    Changed most_unique_reacts_on_a_post

--- src/analytics.py
+++ src/analytics.py
@@ -1,4 +1,3 @@
-from functools import reduce
 from itertools import islice
 from collections import defaultdict, Counter
 import operator
@@ -53,7 +52,7 @@ def get_top_by_value(data, count=5, sort_key=operator.itemgetter(1)):
 # Given a list of message ids, get all the unique words
 # in those messages. Parses escaped channels/users
 # (i.e. UserID -> display_name)
-def get_unique_words( msgs, users, channels):
+def get_unique_words(msgs, users, channels):
 	channel_re = re.compile('(?<=<#)(.*?)(?=>)')
 	user_re = re.compile('(?<=<@)(.*?)(?=>)')
 
@@ -67,7 +66,6 @@ def get_unique_words( msgs, users, channels):
 		msg_text = msgs[msg_id].lower()
 		if not msg_text:
 			continue
-		#msg_text.translate(translator)
 		split_msg = {w.translate(translator) for w in msg_text.split(' ') if w.lower() not in stop_words}
 		print(split_msg)
 		for word in split_msg:
@@ -92,54 +90,6 @@ def react_buzzword(react_name, users, channels, count=5):
 	words = get_unique_words(msgs, users, channels)
 	ret = get_top_by_value(words, count)
 	return ret
-
-def reacts_to_words(users, channels, count=5):
-
-	word_count = {}
-
-	channel_re = re.compile('(?<=<#)(.*?)(?=>)')
-	user_re = re.compile('(?<=<@)(.*?)(?=>)')
-
-	word_to_reacts = {}
-
-	for msg_id in db.get_message_ids():
-		msg_text = db.get_message_text(msg_id)
-		split_msg = set(msg_text.split(' '))
-		reacts_on_msg = db.get_reacts_on_message(msg_id)
-		if not reacts_on_msg:
-			continue
-		for w in split_msg:
-			key = w
-
-			# If theres a channel ID, get the name
-			ch_find = channel_re.search(w)
-			if ch_find:
-				ch_id = ch_find.group(0)
-				if ch_id in channels:
-					key = channels[ch_id]
-
-			# If there's a user ID, get the name
-			user_find = user_re.search(w)
-			if user_find:
-				user_id = user_find.group(0)
-				if user_id in users:
-					key = users[user_id]['display_name']
-
-			if key in word_count:
-				word_count[key] += 1
-			else:
-				word_count[key] = 1
-
-			if key not in word_to_reacts:
-				word_to_reacts[key] = {}
-
-			for react in reacts_on_msg:
-				if react in word_to_reacts[key]:
-					word_to_reacts[key][react] += 1
-				else:
-					word_to_reacts[key][react] = 1
-
-	return word_to_reacts
 
 #Given a condition return the first count number
 #of elements in counter that satisfies the condition
