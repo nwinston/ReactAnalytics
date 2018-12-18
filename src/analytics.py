5cf07019b25c883105195c7458e396ea652a8d9a
nwinston <noahwinston@Noahs-MacBook-Pro-2.local>
2018-12-17 20:49:03 -0500

    refactor

--- src/analytics.py
+++ src/analytics.py
@@ -30,7 +30,7 @@ USER_EXPR = re.compile('(?<=<@)(.*?)(?=>)')
 
 def get_top(f):
     def wrapper(count=5, *args, **kwargs):
-        counter = Counter(f(*args, **kwargs, count=count))
+        counter = Counter(f(*args))
         return dict(counter.most_common(count))
     return wrapper
 
