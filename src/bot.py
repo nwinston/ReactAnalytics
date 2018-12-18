cbcd1ef192496e58052a426db471edd024d89c67
nwinston <noahwinston@Noahs-MacBook-Pro-2.local>
2018-12-17 20:54:11 -0500

    refactor

--- src/bot.py
+++ src/bot.py
@@ -320,11 +320,10 @@ class Bot(object):
     def most_unique_reacts_on_post(self, text):
         channel_id = re.search('(?<=\#)(.*?)(?=\|)', text)
         result_str = ['Messages with most unique reacts:']
-        
+
         result = analytics.most_unique_reacts_on_a_post()
 
-        for msg_id, reacts in result.items():
-            text = db.get_message_text('', msg_id)
+        for msg, reacts in result.items():
             if text:
                 react_str = ''.join([':' + r + ': ' for r in reacts.keys()])
                 result_str.append(text + ' : ' + react_str)
