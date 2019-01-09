b679bfe401ff6c473e507604d39cabba014aa0be
nwinston <noahwinston@Noahs-MacBook-Pro-2.local>
2019-01-09 15:54:48 -0500

    changing db structure

--- src/util.py
+++ src/util.py
@@ -2,7 +2,7 @@ def msg_id_string(channel_id, time_stamp):
     return channel_id + time_stamp
 
 class React:
-    def __init__(self, team_id, channel_id, time_stamp, user_id, react_name):
+    def __init__(self, team_id, channel_id, time_stamp, user_id, name):
         self.team_id = team_id
         self.msg_id = msg_id_string(channel_id,time_stamp)
         self.user_id = user_id
